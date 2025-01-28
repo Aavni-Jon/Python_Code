9d4b78029c8081417f3207b141d25865b9ee1bd8
Aavni-Jon <aavnijon@gmail.com>
2025-01-28 08:21:17 -0800

    Create Ch2_FlowControlLabs.py

---
+++ Ch2_FlowControlLabs.py
@@ -0,0 +1,23 @@
+#8.1.2.2.1 - Chapter 2  - Python - Flow Control - IF/ELIF/ELSE - STEAM Clown
+#Labs from: https://docs.google.com/presentation/d/1nMybtjErJBdFNQ6iDcKxfz-XPAiZnrEsZGtcSN_qJwA/edit#slide=id.gc91dee5b12_0_175 
+
+# Python Lab 1 - Asking Questions - Introduction
+# Replit: https://replit.com/@STEAMClown/Py-Ch2-Lab1-Asking-Questions-Intro#main.py
+print('''
+Challenge # 1
+----------------------------------------------------
+Can you explain?
+
+What do the following mean?
+1) Assignment: Assign values to variables using an assignmnt operator.
+2) Comparison: Ask a question using a comparison operator. If they are equal it will return True, False if they aren't.
+3) Evaluation: Follows operator precedence to do the intended task, such as printing the intended result. 
+4) What is the difference between "=" and "==": "=" is an assignment operator, "==" is a comparison operator.
+5) Boolean: True or False value after performin a comparison or evaluation.
+      
+Hint: https://www.w3schools.com/python/python_operators.asp
+---
+''')
+
+
+
