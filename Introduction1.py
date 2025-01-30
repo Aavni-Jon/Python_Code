28f727eaf1030103ebdda029e1ec12508ce96996
Aavni-Jon <aavnijon@gmail.com>
2025-01-30 08:35:13 -0800

    Create Introduction1.py

---
+++ Introduction1.py
@@ -0,0 +1,16 @@
+#Python Ch1 - Variables, Expressions, Data Types & Operators
+#Slideshow: https://docs.google.com/presentation/d/1VsPztt4kfu9cvprFU1RwUPLttx4nOkxAZymEiCSceHU/edit#slide=id.p
+
+def main():
+  print("Starting The Code Challenge")
+  # Declare Any main() Variables - (Not Global)
+
+  print("Starting <Specific Part> Of Code Challenge")
+  # Enter Your Code Here:
+  print('Enter Your Code Here:')
+  x = 42
+  print(x)
+  print("Ending <Specific Part> Of Code Challenge")
+  print("Ending The Code Challenge")
+
+main()
