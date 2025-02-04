efcab88fcb0eba59f7feb32915b29901be934312
Aavni-Jon <aavnijon@gmail.com>
2025-02-04 08:35:15 -0800

    Create Try_Except2.py

---
+++ Try_Except2.py
@@ -0,0 +1,68 @@
+#Ch 2 - W3-Schools Review - Flow Control - TRY/EXCEPT
+
+def main():
+  print("Starting The Code Challenge")
+  # Declare Any main() Variables - (Not Global)
+
+  print("Starting <Specific Part> Of Code Challenge")
+  # Enter Your Code Here:
+  print('Enter Your Code Here:')
+
+  #Exception Handling
+  try:
+    print(x)  #Exception because x is not defined
+  except:
+    print("An exception occurred")
+
+  #Many Exception
+  try:
+    print(x)
+  except NameError:
+    print("Variable x is not defined")
+  except:
+    print("Something else went wrong")
+
+  #Else - Can us to define a block of code to be executed if no errors were raised
+  try:
+    print("Hello")
+  except:
+    print("Something went wrong")
+  else:
+    print("Nothing went wrong")
+
+  #Finally - If specified, will be executed regardless if the try block raises an error or not.
+  try:
+    print(x)
+  except:
+    print("Something went wrong")
+  finally:
+    print("The 'try except' is finished")
+
+  #This can be useful to close objects and clean up resources:
+  #Try to open and write to a file that is not writable:
+  try:
+    f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+  except:
+    print("Something went wrong when opening the file")
+
+  #Raise an exception - same as throw an exception if a condition occures
+  x = -1
+  if x < 0:
+    raise Exception("Sorry, no numbers below zero")
+
+  #Raise a TypeError if x is not an integer:
+  x = "hello"
+  if not type(x) is int:
+    raise TypeError("Only integers are allowed")
+
+  
+  print("Ending <Specific Part> Of Code Challenge")
+  print("Ending The Code Challenge")
+
+main()
