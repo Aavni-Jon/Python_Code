1c7e58c40ce5f49a74b06f87497098bd9b8e2da7
Aavni-Jon <aavnijon@gmail.com>
2025-02-05 09:58:01 -0800

    Create For_Loops2.py

---
+++ For_Loops2.py
@@ -0,0 +1,25 @@
+#Ch 2 - Flow Control - FOR Loop
+
+def main():
+  print("Starting The Code Challenge")
+  # Declare Any main() Variables - (Not Global)
+
+  print("Starting <Specific Part> Of Code Challenge")
+  # Enter Your Code Here:
+  print('Enter Your Code Here:')
+  x = 42
+  print(x)
+
+  for my_variable in range(1,6) :
+    print(my_variable)
+
+  # Counting Up By +10
+  for my_variable in range(0,101,10) :
+    print(my_variable)
+  print("WooHoo")
+
+
+  print("Ending <Specific Part> Of Code Challenge")
+  print("Ending The Code Challenge")
+
+main()
