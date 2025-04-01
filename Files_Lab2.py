0c6a4f154de1cb890c352a26ec6ddbfbf1d47bdc
Aavni-Jon <aavnijon@gmail.com>
2025-04-01 08:23:08 -0700

    Create Files_Lab2.py

---
+++ Files_Lab2.py
@@ -0,0 +1,41 @@
+#Python - Ch 9 - Files - Lab #2 - More File Handle
+
+# Opening File Handles and reading data from files
+# What happends if the file does not exist?
+# You can get the following file with wget
+# sudo wget -O text-flie-matrix.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-matrix.txt
+# sudo wget -O text-file-mail-very-short.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-mail-very-short.txt
+
+print('''
+This Lab is about opening a file handle, and 
+printing the file handle.
+
+Hint: Check out W3Schools File Handling
+- https://www.w3schools.com/python/python_file_handling.asp
+      ''')
+# -----------------------------
+print('''
+Challenge
+----------------------------------------------------
+
+What happends if the file does not exist? 
+- Try opening a file that does not exist... 
+Like "text-file-matrix2.txt"
+
+Can you open a different file? 
+- Try opening the file "text-file-very-short.txt"
+---
+''')
+# -------------------------------------------------
+print('''Answer to Challenge
+-------------------------------------------------''')
+# Try opening a file that does not exist... Like "matrix2.txt"
+xfile = open('text-file-matrix.txt')
+print(xfile)
+print('''
+-------------------------------------------------''')
+print("When a file doesn't exist then a FileNotFoundError exception is raised")
+yfile = open('text-file-matrix2.txt')
+print(yfile)
+zfile = open('text-file-very-short.txt')
+print(zfile)
