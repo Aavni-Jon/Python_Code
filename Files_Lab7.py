79635e542fe0e5727d16c71521fa211dd10f9962
Aavni-Jon <aavnijon@gmail.com>
2025-04-01 10:22:24 -0700

    Create Files_Lab7.py

---
+++ Files_Lab7.py
@@ -0,0 +1,49 @@
+#Ch 9 - Files - Lab #7 - More Reading & Finding
+
+# Open the text files mbox-short.txt, read in each line, find all the emails, and print out a list of emails.
+# You can get the following file with wget
+# sudo wget -O text-flie-matrix.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-matrix.txt
+# sudo wget -O text-file-mail-very-short.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-mail-very-short.txt
+# sudo wget -O text-file-mail-short.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-mail-short.txt
+# sudo wget -O text-file-mail-long.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Python-Class-STEAM-Clown/refs/heads/main/text-file-mail-long.txt
+
+print('''Open the text files text-file-short.txt, read in each line,
+find all the emails, add them to a new list,
+and print out a list of emails.
+
+#1 - look at each line
+#2 - If the line has an Email in it
+    - How would you find them?
+    - Hint: split the line into a list of "words" then look for emails
+
+  'From: stephen.marquard@uct.ac.za
+Subject: [sakai] svn commit: r39772 - content/branches/sakai_2-5-x/content-impl/impl/src/java/org/sakaiproject/content/impl'    
+
+#3 - strip the email.  Only print a clean email on each output of an email.
+  'stephen.marquard@uct.ac.za'      
+      
+Open the text-file-mail-very-short.txt first, then try the 
+text-file-mail-short.txt befor you try text-file-mail-long.txt
+
+# Add Your Code Here:
+------------------------------------------------
+      ''')
+# Add Your Code Here:
+shortfile = open('text-file-short.txt')
+for line in shortfile:
+  words = line.split()
+  for word in words:
+    foundAt = word.find('@')
+    foundPeriod = word.find('.')
+    if foundAt != -1: and foundPeriod != -1:
+      line=line.rstrip()
+      print(word)
+  longfile = open('text-file-mail-long.txt')
+  for line in longfile:
+  words = line.split()
+  for word in words:
+    foundAt = word.find('@')
+    foundPeriod = word.find('.')
+    if foundAt != -1: and foundPeriod != -1:
+      line=line.rstrip()
+      print(word)
